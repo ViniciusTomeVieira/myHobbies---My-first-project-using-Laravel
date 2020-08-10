6900086ff91063856df56c032ef4a0ff6ccbc2ea
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2020-08-10 14:03:00 -0300

    Login page created with the first migrations added

---
+++ resources/views/info.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('page_title','About this project')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Info') }}</div>
+
+                <div class="card-body">
+                    Chama no ricks
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Olá {{auth()->user()->name ?? ''}} 
                 </div>
 
                 <div class="links">
